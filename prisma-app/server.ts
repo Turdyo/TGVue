0b43ac578b5f8d43016ff2899901f31022b5cfd5
Faust1-2 <63306758+Faust1-2@users.noreply.github.com>
2022-10-25 01:29:57 +0200

    FIxed a bug where endpoint were not accessible

--- prisma-app/server.ts
+++ prisma-app/server.ts
@@ -4,20 +4,20 @@ import { createNewAccount, login } from './Controllers/auth-controller';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // Parse data into json
 
-app.get('api/users/:id', (req, res) => getUserData(req, res));
+app.get('/api/users/:id', (req, res) => getUserData(req, res));
 
-app.put('api/users/:id/updatePwd', async (req, res) => setNewPassword(req, res));
+app.put('/api/users/:id/updatePwd', async (req, res) => setNewPassword(req, res));
 
-app.post('api/users/:id/addTicket',  (req, res) => addTicketToUser(req, res));
+app.post('/api/users/:id/addTicket',  (req, res) => addTicketToUser(req, res));
 
 // Delete tickets first and user then to prevent any issue
-app.delete('api/users/:id/deleteAccount', (req, res) => deleteAccount(req, res))
+app.delete('/api/users/:id/deleteAccount', (req, res) => deleteAccount(req, res))
 
-app.post('api/auth/createUser', (req, res) => createNewAccount(req, res))
+app.post('/api/auth/createUser', (req, res) => createNewAccount(req, res))
 
-app.get('api/auth/login', async (req, res) => login(req, res))
+app.get('/api/auth/login', async (req, res) => login(req, res))
 
 app.listen(3000, () =>
   console.log(`
