1dd677c1a7b62ed2563e07e29d96526159f9d229
Faust1-2 <63306758+Faust1-2@users.noreply.github.com>
2022-10-24 19:59:49 +0200

    User creation and data retrieving

--- prisma-app/src/index.ts
+++ prisma-app/src/index.ts
@@ -6,11 +6,52 @@ const app = express()
 
 app.use(express.json());
 
-app.get('/users:id', async (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const { id } = req.params;
-  const users = await prisma.user.findMany();
-  res.json(users)
+  prisma.user.findUnique({
+    where:{
+      id: String(id),
+    },
+    select:{
+      firstName: true,
+      lastName: true,
+      email:true,
+      tickets: {
+        select: {
+          start_station:true,
+          end_station:true,
+          date: true,
+          price: true
+        }
+      }
+    }
+  })
+  .then(user => res.json(user))
+  .catch(e => {
+    console.log(e);
+  });
+  
 });
+
+app.post('/createUser', async (req, res) => {
+  const body = req.body;
+  console.log(body);
+  
+  prisma.user.create({
+    data: {
+      email: body.email,
+      lastName: body.lastName,
+      firstName: body.firstName,
+      password: body.password
+    }
+  })
+  .then(newUser => res.json(newUser))
+  .catch(e => {
+    console.log(e)
+    res.json({"error":"Something went wrong..."})
+  });
+
+})
 
 app.listen(3000, () =>
   console.log(`
