028353dfc48247733eaa7a6b4f3f0523dca9db76
Faust1-2 <63306758+Faust1-2@users.noreply.github.com>
2022-10-25 00:21:47 +0200

    Structure modification (with controllers for data manipulation) and auth working

---
+++ prisma-app/server.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+import { getUserData, setNewPassword, addTicketToUser, deleteAccount } from './Controllers/user-controller'
+import { createNewAccount, login } from './Controllers/auth-controller';
+
+const prisma = new PrismaClient();
+const app = express();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
+
+app.use(express.json());
+
+app.get('api/users/:id', (req, res) => getUserData(req, res));
+
+app.put('api/users/:id/updatePwd', async (req, res) => setNewPassword(req, res));
+
+app.post('api/users/:id/addTicket',  (req, res) => addTicketToUser(req, res));
+
+
+// Delete tickets first and user then to prevent any issue
+app.delete('api/users/:id/deleteAccount', (req, res) => deleteAccount(req, res))
+
+app.post('api/auth/createUser', (req, res) => createNewAccount(req, res))
+
+app.get('api/auth/login', async (req, res) => login(req, res))
+
+app.listen(3000, () =>
+  console.log(`
+🚀 Server ready at: http://localhost:3000
+`),
+)
